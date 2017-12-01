8bb9c4ca1b87914c8f4c952ddc11fd1318ccffad
Rod Johnson <rod@atomist.com>
2017-11-30 19:29:31 -0800

    Added POM verification

---
+++ src/commands/editor/spring/verifyPom.ts
@@ -0,0 +1,52 @@
+import { HandleCommand } from "@atomist/automation-client";
+import { BaseEditorParameters } from "@atomist/automation-client/operations/edit/BaseEditorParameters";
+import { reviewerHandler } from "@atomist/automation-client/operations/review/reviewerToCommand";
+import { clean, ProjectReview } from "@atomist/automation-client/operations/review/ReviewResult";
+import { Project } from "@atomist/automation-client/project/Project";
+import { SpringBootStarter, SpringBootTags } from "./springConstants";
+import { promisify } from "util";
+
+import * as _ from "lodash";
+
+const xml2js = require("xml2js");
+
+/**
+ * Find all non specific, old style @RequestMapping annotations
+ * @param {Project} p project to search
+ * location of source tree.
+ */
+export function verifyPom(p: Project): Promise<ProjectReview> {
+    return p.findFile("pom.xml")
+        .then(f => f.getContent())
+        .then(content => {
+                const parser = new xml2js.Parser();
+                return promisify(parser.parseString)(content)
+                    .then(parsed => {
+                        const parent = _.get(parsed, "project.parent");
+                        if (!!parent && parent.artifactId.toString() === SpringBootStarter) {
+                            return clean(p.id);
+                        }
+                        // TODO check dependency management
+                        return {
+                            repoId: p.id,
+                            comments: [
+                                {
+                                    severity: "warn",
+                                    comment: `POM should extend ${SpringBootStarter} or use Spring Boot dependency management`,
+                                    sourceLocation: {path: "pom.xml", offset: -1,},
+                                }],
+                        }
+                    });
+            }
+        ).catch(err => clean(p.id));
+}
+
+export const verifyPomCommand: HandleCommand =
+    reviewerHandler(() => verifyPom,
+        BaseEditorParameters,
+        "VerifyPOM",
+        {
+            tags: SpringBootTags,
+            intent: "verify pom",
+        },
+    );
---
+++ test/commands/editor/spring/verifyPomTest.ts
@@ -0,0 +1,55 @@
+import { InMemoryProject } from "@atomist/automation-client/project/mem/InMemoryProject";
+
+import * as assert from "power-assert";
+import {
+    findNonSpecificMvcAnnotations,
+    NonSpecificMvcAnnotation,
+} from "../../../../src/commands/editor/spring/findNonSpecificMvcAnnotations";
+import { verifyPom } from "../../../../src/commands/editor/spring/verifyPom";
+import { springBootPom } from "../../reviewer/maven/Poms";
+
+describe("verify POM", () => {
+
+    it("finds none in empty project", done => {
+        const p = new InMemoryProject();
+        verifyPom(p)
+            .then(r => {
+                assert(r.comments.length === 0);
+                done();
+            }).catch(done);
+    });
+
+    it("finds none in a Java file", done => {
+        const path = "src/main/java/Foo.java";
+        const content = "public class MyApp { private String dog; @Autowired public MyApp(Thing t) {} }";
+        const p = InMemoryProject.of(
+            { path, content });
+        verifyPom(p)
+            .then(r => {
+                assert(r.comments.length === 0);
+                done();
+            }).catch(done);
+    });
+
+    it("accepts POM with valid parent", done => {
+        const content = springBootPom("1.5.9");
+        const p = InMemoryProject.of(
+            { path: "pom.xml", content });
+        verifyPom(p)
+            .then(r => {
+                assert(r.comments.length === 0);
+                done();
+            }).catch(done);
+    });
+
+    it("finds problem", done => {
+        const p = InMemoryProject.of(
+            { path: "pom.xml", content: "<xml></xml>" });
+        verifyPom(p)
+            .then(r => {
+                assert(r.comments.length === 1);
+                assert(r.comments[0].sourceLocation.path === "pom.xml");
+                done();
+            }).catch(done);
+    });
+});
