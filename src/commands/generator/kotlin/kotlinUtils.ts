9a8c2d41c5f9175d193cb05fe966180824b97d23
Rod Johnson <rod@atomist.com>
2018-04-29 11:03:22 +1000

    Remove Travis file. Just one Kotlin path

--- src/commands/generator/kotlin/kotlinUtils.ts
+++ src/commands/generator/kotlin/kotlinUtils.ts
@@ -14,11 +14,7 @@
  * limitations under the License.
  */
 
-import { KotlinFileParser } from "@atomist/antlr/tree/ast/antlr/kotlin/KotlinFileParser";
-import { logger } from "@atomist/automation-client/internal/util/logger";
 import { ProjectAsync } from "@atomist/automation-client/project/Project";
-import { findFileMatches } from "@atomist/automation-client/tree/ast/astUtils";
-import { evaluateScalarValue } from "@atomist/tree-path/path/expressionEngine";
 import { SpringBootProjectStructure } from "../spring/SpringBootProjectStructure";
 
 export const AllKotlinFiles = "src/**/kotlin/**/*.kt";
@@ -32,21 +28,5 @@ export const KotlinSourceFiles = "src/main/kotlin/**/*.kt";
  * @return {Promise<SpringBootProjectStructure>}
  */
 export function inferFromKotlinSource(p: ProjectAsync): Promise<SpringBootProjectStructure> {
-    // Run a path expression against the Kotlin ANTLR grammar
-    return findFileMatches(p, KotlinFileParser, KotlinSourceFiles,
-        "//classDeclaration[//annotation[@value='@SpringBootApplication']]/simpleIdentifier")
-        .then(files => {
-            if (files.length !== 1) {
-                return undefined;
-            } else {
-                const f = files[0];
-                const appClass = f.matches[0].$value;
-                // Use the AST from the matching file to extract the package
-                const packageName = evaluateScalarValue(f.fileNode, "//packageHeader/identifier");
-                logger.info(`Spring Boot inference: packageName=${packageName}, appClass=${appClass}`);
-                return (packageName && appClass) ?
-                    new SpringBootProjectStructure(packageName, appClass, f.file) :
-                    undefined;
-            }
-        });
+    return SpringBootProjectStructure.inferFromKotlinSource(p);
 }
